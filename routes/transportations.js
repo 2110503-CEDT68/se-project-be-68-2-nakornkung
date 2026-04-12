25d21132f75f9a0a31cec8fbc80507811797542d
XRXN-pi <irin.thir@gmail.com>
2026-04-12 19:43:47 +0700

    routes/transporatitation

--- routes/transportations.js
+++ routes/transportations.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const {} = require('../controllers/transportations'); 
+const { getTransportations } = require('../controllers/transportations'); 
+const { protect, authorize } = require('../middleware/auth');
+
+router.route('/')
+  .get(protect, getTransportations)
+  .post(protect, authorize('admin'), addTransportation);
+router.route('/:id')
+  .get(protect, getTransportation)
+  .put(protect, authorize('admin'), updateTransportation)
+  .delete(protect, authorize('admin'), deleteTransportation);
+
 
 module.exports = router;
